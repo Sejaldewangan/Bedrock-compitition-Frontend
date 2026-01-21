960271ebda338f56d21cb1f9f6e2fb6bfb2ff350
Sejaldewangan <shubham.dewangan.3440@gmail.com>
2026-01-21 06:49:44 +0530

    Api health checkup

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://bedrock-compitition-backend-render.onrender.com/";
+const API_BASE_URL= "https://bedrock-compitition-backend-render.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
